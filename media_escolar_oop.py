842b1609f5d1b8df46adb0fcb1bc54575095b024
LucasAMiranda <lucasa.miranda@hotmail.com>
2021-04-24 18:17:02 -0300

    [update - 2] - media escolar orientado a objetos

--- media_escolar_oop.py
+++ media_escolar_oop.py
@@ -1,28 +1,33 @@
 class Escola:
-
-    def __init__(self, nota, nomeAluno, media):
+    def __init__(self, nota, media, nomeAluno):
         self.nota = nota
         self.nomeAluno = nomeAluno
         self.media = media
-        self.listarNotas(5)
-        self.mostrarMedia()
+        self.listarNotas()
 
-    def listarNotas(self,  nota):
-        self.notas = []
-        self.notas.append(self.nota)
-        self.nota.split(",")
-        for notas in self.notas:
-            if notas['nota'] == nota:
-                print(f"As notas são: {nota}")
+    def listarNotas(self):
+        contador = 0
+        for nome in self.nomeAluno:
+            self.mostrarMedia(nome, self.nota[contador]) # nome e a nota da lista que vão ser armazenados
+            contador += 1
 
-    def mostrarMedia(self):
-        self.media += self.nota / 3
-        if self.media >= 7:
-            print("Aluno foi aprovado")
+    def mostrarMedia(self, nome, nota):
+        if nota >= self.media:
+            print("Aluno: {},\nnota: {},\n::aprovado".format(nome, nota))
         else:
-            print("Aluno foi reprovado")
+            print("Aluno: {},\nnota: {},\n::reprovado".format(nome, nota))
 
 
-if __name__ == 'main':
-    escola = Escola(nota=7, media=4, nomeAluno='Mateus')
-    print(escola)
+contador = int(input('Digite quantos alunos vão ter: '))
+num = 0
+nomeA = []
+notaA = []
+
+while num < contador:
+    nome = input('Digite o nome: ')
+    nota = int(input('Digite a nota: '))
+    nomeA.append(nome)
+    notaA.append(nota)
+    num += 1
+
+escola = Escola(nota=notaA, media=4, nomeAluno=nomeA)
